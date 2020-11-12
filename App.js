db1592d4a742b4a00cc0120fb6cecc53464dc7e4
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2020-11-12 23:46:49 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [goals, setGoals] = useState([])
+
+  const goalInputHandler = (text) => {
+    setEnteredGoal(text)
+  }
+
+  const addButtonGoalHandler = () => {
+    // setGoals([...goals, enteredGoal])
+    setGoals(xyz => [
+      ...xyz, 
+      {id:Math.random().toString(), value:enteredGoal}
+    ])
+    console.log(enteredGoal)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+    <ScrollView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Write here ..."
+        value={enteredGoal}
+        onChangeText={goalInputHandler}
+      />
+      <Button
+        style={styles.button}
+        title="add task"
+        onPress={addButtonGoalHandler}
+      />
+      <View style={styles.mainList}>
+        {goals.map((goal) => (
+          <FlatList
+            data={goals}
+            keyExtractor={(item, index)=>item.id}
+            renderItem={(item) => (
+              <View style={styles.enteredGoal}>
+                <Text >
+                  {item.item.value}
+                </Text>
+              </View>
+            
+            )}
+          >
+
+          </FlatList>
+        ))}
+      </View>
+
+    </ScrollView>
+  )
 }
 
+
+
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "grey",
+    // marginVertical:10,
+    marginBottom: 20,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 42,
+
+    // justifyContent: 'center',
+    // alignItems:'center',
+    padding: 10
+
   },
+  enteredGoal: {
+    borderColor: 'grey',
+    borderWidth: 2,
+    padding: 5,
+    marginBottom: 10,
+    backgroundColor: 'pink'
+
+
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 2,
+    textAlign: 'center',
+    marginBottom: 5,
+
+  },
+  mainList: {
+    marginTop: 20,
+  }
 });
